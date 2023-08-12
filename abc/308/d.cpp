5c03c0908a656b36d3dc8e3d37b463c32fbf4357
attyoh <business.832@outlook.jpexit()>
2023-08-12 17:42:26 +0900

    fix d.cpp

--- abc/308/d.cpp
+++ abc/308/d.cpp
@@ -1,9 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i,n) for (int i = 0; i < (n); ++i)
-#define nrep(i,s,n) for(int i = (s); i < (n); ++i)
-using P = pair<int, int>;
-using ll = long long;
 
 const int di[] = {1,0,-1,0};
 const int dj[] = {0,1,0,-1};
