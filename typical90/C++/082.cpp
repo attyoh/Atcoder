b6cf177d0331454910c63f1e123d7802bc28d58e
attyoh <business.832@outlook.jpexit()>
2023-08-24 14:39:12 +0900

    create 084.cpp

---
+++ typical90/C++/082.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using ll = unsigned long long;
+
+ll mod = 1000000007;
+ll l, r;
+
+int main() {
+    
+    return 0;
+}
