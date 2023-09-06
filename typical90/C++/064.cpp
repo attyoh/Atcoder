dc8013c63eac357ed9ac04227c0998d034141860
attyoh <business.832@outlook.jpexit()>
2023-09-06 22:38:22 +0900

    fix 064.cpp

--- typical90/C++/064.cpp
+++ typical90/C++/064.cpp
@@ -4,26 +4,27 @@ using namespace std;
 using ll = long long;
 
 int main() {
-    ll n, q;
+	ll n, q;
 	cin >> n >> q;
-    vector<ll> a(n), b(n);
+	vector<ll> a(n);
 	rep(i,n) cin >> a[i];
 
 	ll ans = 0;
+	vector<ll> b(n);
 	rep(i,n-1) {
 		b[i] = a[i+1] - a[i];
 		ans += abs(b[i]);
 	}
-	
+
 	rep(i,q) {
-        ll l, r, v;
-        cin >> l >> r >> v;
-        --l; --r;
-		ll mae = abs(b[l-1]) + abs(b[r]);
+		ll l, r, v;
+		cin >> l >> r >> v;
+		--l; --r;
+		ll pre = abs(b[l-1]) + abs(b[r]);
 		if (l > 0) b[l-1] += v;
 		if (r < n-1) b[r] -= v;
-		ll ato = abs(b[l-1]) + abs(b[r]);
-		ans += (ato - mae);
+		ll aft = abs(b[l-1]) + abs(b[r]);
+		ans += aft - pre;
 		cout << ans << endl;
 	}
 	return 0;
