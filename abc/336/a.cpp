662fd602c6396834a9f07d64f3703be0e8c4a77d
attyoh <business.832@outlook.jpexit()>
2024-01-28 23:15:18 +0900

    fix a.cpp

--- abc/336/a.cpp
+++ abc/336/a.cpp
@@ -1,11 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i,n) for (int i = 0; i < (n); ++i)
-#define nrep(i,s,n) for(int i = (s); i < (n); ++i)
-using P = pair<int, int>;
-using ll = long long;
 
 int main() {
-    
+    int n;
+    cin >> n;
+    cout << "L" << string(n,'o') << "ng" << endl;
     return 0;
 }
