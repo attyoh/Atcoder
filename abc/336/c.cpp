6ca3f59340aff3b0775fa554a72d0df03e67da63
attyoh <business.832@outlook.jpexit()>
2024-02-03 20:39:31 +0900

    fix c.cpp

--- abc/336/c.cpp
+++ abc/336/c.cpp
@@ -1,11 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i,n) for (int i = 0; i < (n); ++i)
-#define nrep(i,s,n) for(int i = (s); i < (n); ++i)
-using P = pair<int, int>;
 using ll = long long;
 
 int main() {
-    
+    ll n;
+    cin >> n;
+    n--;
+
+    if (n == 0) {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    string s;
+    while(n) {
+        s += (n%5)*2 + '0';
+        n /= 5;
+    }
+
+    reverse(s.begin(), s.end());
+    cout << s << endl;
     return 0;
 }
