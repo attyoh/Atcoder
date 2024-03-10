1b16eec419087f09f09fdbe7acb2f63006417a8e
attyoh <business.832@outlook.jpexit()>
2024-03-10 22:47:17 +0900

    create a.cpp

--- abc/335/a.cpp
+++ abc/335/a.cpp
@@ -1,11 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i,n) for (int i = 0; i < (n); ++i)
-#define nrep(i,s,n) for(int i = (s); i < (n); ++i)
-using P = pair<int, int>;
-using ll = long long;
 
 int main() {
-    
+    string s;
+    cin >> s;
+    s.back() = '4';
+    cout << s << endl;
     return 0;
 }
